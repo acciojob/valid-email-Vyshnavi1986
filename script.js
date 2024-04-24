60f7b43be15f6fda65a505331f43754ae0981f92
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-24 15:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function validEmail(str) {
-  //your JS code here.
+ const email = str; 
+const emailPattern =  
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; 
+//const isValid = emailPattern.test(email); 
+console.log(emailPattern.test(email));
 }
 
 // Do not change the code below.
+
 const str = prompt("Enter an email address.");
-alert(validEmail(str));
+validEmail(str);
+// Example usage:
+console.log(validEmail("example@example.com")); // true
+console.log(validEmail("example@.com")); // false
+console.log(validEmail("example.com")); // false
+console.log(validEmail("example@example")); // false
